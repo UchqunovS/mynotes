e6be52f5fcf94d7bc01aa7cf104f8135decf8352
Sardor <sardor@kali.sarkali.com>
2023-12-11 17:21:27 +0500

    step-9 fixed

--- lib/services/auth/firebase_auth_provider.dart
+++ lib/services/auth/firebase_auth_provider.dart
@@ -47,7 +47,7 @@ class FirebaseAuthProvider implements AuthProvider {
     if (user != null) {
       return AuthUser.fromFirebase(user);
     } else {
-      throw UserNotLoggedInAuthException();
+      return null;
     }
   }
 
