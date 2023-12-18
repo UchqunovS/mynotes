c2ad7766a119f114567f02ec6586bd25e6361531
UchqunovS <sardoruchqunov@gmail.com>
2023-12-19 01:59:56 +0500

    step-17

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants/routes.dart';
 import 'package:notes_app/services/auth/auth_service.dart';
-import 'package:notes_app/views/login_view.dart';
-import 'package:notes_app/views/notes/new_note_view.dart';
+import 'package:notes_app/views/authentication/login_view.dart';
+import 'package:notes_app/views/notes/create_update_note_view.dart';
 import 'package:notes_app/views/notes/notes_view.dart';
-import 'package:notes_app/views/register_view.dart';
-import 'package:notes_app/views/verify_email_view.dart';
+import 'package:notes_app/views/authentication/register_view.dart';
+import 'package:notes_app/views/authentication/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +17,7 @@ void main() {
         appBarTheme: const AppBarTheme(
           centerTitle: true,
         ),
+        useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
@@ -25,7 +26,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/services/crud/notes_service.dart';
 import 'package:notes_app/utilities/dialogs/delete_dialog.dart';
 
-typedef DeleteNoteCallBack = void Function(DatabaseNote note);
+typedef NoteCallBack = void Function(DatabaseNote note);
 
 class NotesListView extends StatelessWidget {
   final List<DatabaseNote> notes;
-  final DeleteNoteCallBack onDeleteNote;
+  final NoteCallBack onDeleteNote;
+  final NoteCallBack onTap;
+
   const NotesListView({
     super.key,
     required this.notes,
     required this.onDeleteNote,
+    required this.onTap,
   });
 
   @override
@@ -20,6 +23,9 @@ class NotesListView extends StatelessWidget {
       itemBuilder: (context, index) {
         final note = notes[index];
         return ListTile(
+          onTap: () {
+            onTap(note);
+          },
           title: Text(
             note.text,
             maxLines: 1,
